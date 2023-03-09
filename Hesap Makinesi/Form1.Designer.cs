d89f75b2be0dba210163aa5d48554b351ff1d449
Naci Karataş <Naci Karata@NACIPC>
2023-03-09 15:15:58 +0300

    Proje dosyası ekle.

---
+++ Hesap Makinesi/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace Hesap_Makinesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.yedi = new System.Windows.Forms.Button();
+            this.sekiz = new System.Windows.Forms.Button();
+            this.dokuz = new System.Windows.Forms.Button();
+            this.btnbolme = new System.Windows.Forms.Button();
+            this.btnC = new System.Windows.Forms.Button();
+            this.dort = new System.Windows.Forms.Button();
+            this.bes = new System.Windows.Forms.Button();
+            this.altı = new System.Windows.Forms.Button();
+            this.btncarpma = new System.Windows.Forms.Button();
+            this.btnBackSpace = new System.Windows.Forms.Button();
+            this.bir = new System.Windows.Forms.Button();
+            this.iki = new System.Windows.Forms.Button();
+            this.uc = new System.Windows.Forms.Button();
+            this.btncıkartma = new System.Windows.Forms.Button();
+            this.btnters = new System.Windows.Forms.Button();
+            this.btnvirgul = new System.Windows.Forms.Button();
+            this.sıfır = new System.Windows.Forms.Button();
+            this.btnTopla = new System.Windows.Forms.Button();
+            this.btnEsittir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI Black", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.ForeColor = System.Drawing.Color.Crimson;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(470, 58);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // yedi
+            // 
+            this.yedi.Location = new System.Drawing.Point(12, 91);
+            this.yedi.Name = "yedi";
+            this.yedi.Size = new System.Drawing.Size(86, 67);
+            this.yedi.TabIndex = 1;
+            this.yedi.Text = "7";
+            this.yedi.UseVisualStyleBackColor = true;
+            this.yedi.Click += new System.EventHandler(this.Butonlar);
+            // 
+            // sekiz
+            // 
+            this.sekiz.Location = new System.Drawing.Point(104, 91);
+            this.sekiz.Name = "sekiz";
+            this.sekiz.Size = new System.Drawing.Size(86, 67);
+            this.sekiz.TabIndex = 2;
+            this.sekiz.Text = "8";
+            this.sekiz.UseVisualStyleBackColor = true;
+            this.sekiz.Click += new System.EventHandler(this.Butonlar);
+            // 
+            // dokuz
+            // 
+            this.dokuz.Location = new System.Drawing.Point(196, 91);
+            this.dokuz.Name = "dokuz";
+            this.dokuz.Size = new System.Drawing.Size(86, 67);
+            this.dokuz.TabIndex = 3;
+            this.dokuz.Text = "9";
+            this.dokuz.UseVisualStyleBackColor = true;
+            this.dokuz.Click += new System.EventHandler(this.Butonlar);
+            // 
+            // btnbolme
+            // 
+            this.btnbolme.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnbolme.Location = new System.Drawing.Point(288, 91);
+            this.btnbolme.Name = "btnbolme";
+            this.btnbolme.Size = new System.Drawing.Size(86, 67);
+            this.btnbolme.TabIndex = 4;
+            this.btnbolme.Text = "/";
+            this.btnbolme.UseVisualStyleBackColor = true;
+            this.btnbolme.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btnC
+            // 
+            this.btnC.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnC.Location = new System.Drawing.Point(380, 91);
+            this.btnC.Name = "btnC";
+            this.btnC.Size = new System.Drawing.Size(86, 67);
+            this.btnC.TabIndex = 5;
+            this.btnC.Text = "C";
+            this.btnC.UseVisualStyleBackColor = true;
+            this.btnC.Click += new System.EventHandler(this.btnC_Click);
+            // 
+            // dort
+            // 
+            this.dort.Location = new System.Drawing.Point(12, 164);
+            this.dort.Name = "dort";
+            this.dort.Size = new System.Drawing.Size(86, 67);
+            this.dort.TabIndex = 10;
+            this.dort.Text = "4";
+            this.dort.UseVisualStyleBackColor = true;
+            this.dort.Click += new System.EventHandler(this.Butonlar);
+            // 
+            // bes
+            // 
+            this.bes.Location = new System.Drawing.Point(104, 164);
+            this.bes.Name = "bes";
+            this.bes.Size = new System.Drawing.Size(86, 67);
+            this.bes.TabIndex = 9;
+            this.bes.Text = "5";
+            this.bes.UseVisualStyleBackColor = true;
+            this.bes.Click += new System.EventHandler(this.Butonlar);
+            // 
+            // altı
+            // 
+            this.altı.Location = new System.Drawing.Point(196, 164);
+            this.altı.Name = "altı";
+            this.altı.Size = new System.Drawing.Size(86, 67);
+            this.altı.TabIndex = 8;
+            this.altı.Text = "6";
+            this.altı.UseVisualStyleBackColor = true;
+            this.altı.Click += new System.EventHandler(this.Butonlar);
+            // 
+            // btncarpma
+            // 
+            this.btncarpma.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btncarpma.Location = new System.Drawing.Point(288, 164);
+            this.btncarpma.Name = "btncarpma";
+            this.btncarpma.Size = new System.Drawing.Size(86, 67);
+            this.btncarpma.TabIndex = 7;
+            this.btncarpma.Text = "*";
+            this.btncarpma.UseVisualStyleBackColor = true;
+            this.btncarpma.Click += new System.EventHandler(this.btncarpma_Click);
+            // 
+            // btnBackSpace
+            // 
+            this.btnBackSpace.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnBackSpace.Location = new System.Drawing.Point(380, 164);
+            this.btnBackSpace.Name = "btnBackSpace";
+            this.btnBackSpace.Size = new System.Drawing.Size(86, 67);
+            this.btnBackSpace.TabIndex = 6;
+            this.btnBackSpace.Text = "<---";
+            this.btnBackSpace.UseVisualStyleBackColor = true;
+            this.btnBackSpace.Click += new System.EventHandler(this.btnBackSpace_Click);
+            // 
+            // bir
+            // 
+            this.bir.Location = new System.Drawing.Point(12, 237);
+            this.bir.Name = "bir";
+            this.bir.Size = new System.Drawing.Size(86, 67);
+            this.bir.TabIndex = 15;
+            this.bir.Text = "1";
+            this.bir.UseVisualStyleBackColor = true;
+            this.bir.Click += new System.EventHandler(this.Butonlar);
+            // 
+            // iki
+            // 
+            this.iki.Location = new System.Drawing.Point(104, 237);
+            this.iki.Name = "iki";
+            this.iki.Size = new System.Drawing.Size(86, 67);
+            this.iki.TabIndex = 14;
+            this.iki.Text = "2";
+            this.iki.UseVisualStyleBackColor = true;
+            this.iki.Click += new System.EventHandler(this.Butonlar);
+            // 
+            // uc
+            // 
+            this.uc.Location = new System.Drawing.Point(196, 237);
+            this.uc.Name = "uc";
+            this.uc.Size = new System.Drawing.Size(86, 67);
+            this.uc.TabIndex = 13;
+            this.uc.Text = "3";
+            this.uc.UseVisualStyleBackColor = true;
+            this.uc.Click += new System.EventHandler(this.Butonlar);
+            // 
+            // btncıkartma
+            // 
+            this.btncıkartma.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btncıkartma.Location = new System.Drawing.Point(288, 237);
+            this.btncıkartma.Name = "btncıkartma";
+            this.btncıkartma.Size = new System.Drawing.Size(86, 67);
+            this.btncıkartma.TabIndex = 12;
+            this.btncıkartma.Text = "-";
+            this.btncıkartma.UseVisualStyleBackColor = true;
+            this.btncıkartma.Click += new System.EventHandler(this.btncıkartma_Click);
+            // 
+            // btnters
+            // 
+            this.btnters.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnters.Location = new System.Drawing.Point(12, 310);
+            this.btnters.Name = "btnters";
+            this.btnters.Size = new System.Drawing.Size(86, 83);
+            this.btnters.TabIndex = 11;
+            this.btnters.Text = "+/-";
+            this.btnters.UseVisualStyleBackColor = true;
+            this.btnters.Click += new System.EventHandler(this.btnters_Click);
+            // 
+            // btnvirgul
+            // 
+            this.btnvirgul.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnvirgul.Location = new System.Drawing.Point(104, 310);
+            this.btnvirgul.Name = "btnvirgul";
+            this.btnvirgul.Size = new System.Drawing.Size(86, 81);
+            this.btnvirgul.TabIndex = 16;
+            this.btnvirgul.Text = ",";
+            this.btnvirgul.UseVisualStyleBackColor = true;
+            this.btnvirgul.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // sıfır
+            // 
+            this.sıfır.Location = new System.Drawing.Point(196, 310);
+            this.sıfır.Name = "sıfır";
+            this.sıfır.Size = new System.Drawing.Size(86, 81);
+            this.sıfır.TabIndex = 17;
+            this.sıfır.Text = "0";
+            this.sıfır.UseVisualStyleBackColor = true;
+            this.sıfır.Click += new System.EventHandler(this.Butonlar);
+            // 
+            // btnTopla
+            // 
+            this.btnTopla.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnTopla.Location = new System.Drawing.Point(288, 310);
+            this.btnTopla.Name = "btnTopla";
+            this.btnTopla.Size = new System.Drawing.Size(86, 81);
+            this.btnTopla.TabIndex = 18;
+            this.btnTopla.Text = "+";
+            this.btnTopla.UseVisualStyleBackColor = true;
+            this.btnTopla.Click += new System.EventHandler(this.button18_Click);
+            // 
+            // btnEsittir
+            // 
+            this.btnEsittir.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnEsittir.Location = new System.Drawing.Point(380, 237);
+            this.btnEsittir.Name = "btnEsittir";
+            this.btnEsittir.Size = new System.Drawing.Size(86, 156);
+            this.btnEsittir.TabIndex = 19;
+            this.btnEsittir.Text = "=";
+            this.btnEsittir.UseVisualStyleBackColor = true;
+            this.btnEsittir.Click += new System.EventHandler(this.button19_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 403);
+            this.Controls.Add(this.btnEsittir);
+            this.Controls.Add(this.btnTopla);
+            this.Controls.Add(this.sıfır);
+            this.Controls.Add(this.btnvirgul);
+            this.Controls.Add(this.bir);
+            this.Controls.Add(this.iki);
+            this.Controls.Add(this.uc);
+            this.Controls.Add(this.btncıkartma);
+            this.Controls.Add(this.btnters);
+            this.Controls.Add(this.dort);
+            this.Controls.Add(this.bes);
+            this.Controls.Add(this.altı);
+            this.Controls.Add(this.btncarpma);
+            this.Controls.Add(this.btnBackSpace);
+            this.Controls.Add(this.btnC);
+            this.Controls.Add(this.btnbolme);
+            this.Controls.Add(this.dokuz);
+            this.Controls.Add(this.sekiz);
+            this.Controls.Add(this.yedi);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "HESAP MAKİNEM";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button yedi;
+        private Button sekiz;
+        private Button dokuz;
+        private Button btnbolme;
+        private Button btnC;
+        private Button dort;
+        private Button bes;
+        private Button altı;
+        private Button btncarpma;
+        private Button btnBackSpace;
+        private Button bir;
+        private Button iki;
+        private Button uc;
+        private Button btncıkartma;
+        private Button btnters;
+        private Button btnvirgul;
+        private Button sıfır;
+        private Button btnTopla;
+        private Button btnEsittir;
+    }
+}
